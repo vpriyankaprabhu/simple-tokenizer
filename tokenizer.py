26e08c9ffdba83cc656f4257df57742e490c5d6c
Priyanka Prabhu <vpriyankaprabhu@gmail.com>
2025-04-25 09:23:58 +0530

    updated example

--- tokenizer.py
+++ tokenizer.py
@@ -3,5 +3,5 @@ import tiktoken
 #enc = tiktoken.get_encoding("cl100k_base")
 enc = tiktoken.get_encoding("gpt2")
 tokens = enc.encode("Hello World !")
-print(tokens)  # Output: [12643, 29278, 318, 11722, 0]
+print(tokens)  # Output: [15496, 2159, 5145]
 print([enc.decode([t]) for t in tokens])
