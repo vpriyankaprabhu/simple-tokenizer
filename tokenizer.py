f6554844e113f11f07613cde6ea15d71d2de29b7
Priyanka Prabhu <vpriyankaprabhu@gmail.com>
2025-04-25 09:23:04 +0530

    updated example

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,7 @@
 import tiktoken
 
-enc = tiktoken.get_encoding("cl100k_base")
-tokens = enc.encode("ChatGPT is awesome!")
+#enc = tiktoken.get_encoding("cl100k_base")
+enc = tiktoken.get_encoding("gpt2")
+tokens = enc.encode("Hello World !")
 print(tokens)  # Output: [12643, 29278, 318, 11722, 0]
 print([enc.decode([t]) for t in tokens])
